21c8ada00c68826b7a4d549134fdca4a0b95eaee
Admin <rameshkror@gmail.com>
2018-11-19 14:20:57 +0530

    ICS Admin Panel Added

---
+++ README.md
@@ -0,0 +1,46 @@
+ICS Admin Panel
+===================
+
+Ruby on Rails
+-------------
+
+This application requires:
+
+- Ruby 2.2.2
+- Rails 4.2.4
+
+
+Getting Started
+---------------
+
+To start using this Admin panel, you only have to do the typical ***Rails*** things:
+
+* Install ***Ruby*** version 2.2.2 (using [RVM](https://github.com/rvm/rvm) or [RBenv](https://github.com/sstephenson/rbenv) or whatever).
+
+* Clone the repo and do the ***bundle install*** thing:
+
+```sh
+user@computer:~$ cd ics_admin_panel
+user@computer:/ics_admin_panel$ bundle install
+user@computer:/ics_admin_panel$ rake db:setup
+```
+
+* When all this finish, you're ready to launch the app!
+
+```sh
+user@computer:/ics_admin_panel$ rails s
+```
+
+* Open your web browser and go to [http://localhost:3000](http://localhost:3000) like in all the regular ***Rails*** apps.
+
+Contributor
+------------
+
+1. Rami - rameshkror@gmail.com
+
+
+License
+-------
+
+**ICS Admin Panel** is released under the [MIT License](http://www.opensource.org/licenses/MIT).
+# ics_admin_panel
---
+++ app/controllers/iot_data_controller.rb
@@ -0,0 +1,203 @@
+class IotDataController < ApplicationController
+  # before_filter :authenticate_user!
+  skip_before_filter :authenticate_user!
+  # protect_from_forgery with: :null_session
+  before_action :set_iot_datum, only: [:show, :edit, :update, :destroy]
+
+  # GET /iot_data
+  # GET /iot_data.json
+  def index
+    device_id = params[:device_id] if params[:device_id]
+    count = params[:count] if params[:count]
+    part = params[:part_number] if params[:part_number]
+    @iot_data = IotDatum.where("device_id = ? and count = ? and part_number = ?", params[:device_id],params[:count], params[:part_number]) if device_id && count && part
+      
+      if params[:status] && params[:device_id]
+        @iot_datas = IotDatum.where("status = ? and device_id = ?", 'Processing', params[:device_id]) 
+        render json: @iot_datas
+      end
+      
+    if device_id != nil && part != nil
+      puts "1 begin"
+      status = 'Processing'
+      @iot_dataa = IotDatum.find_by(device_id: device_id, part_number: part, status: status) 
+      @iot_dataa.part_number
+      target = @iot_dataa.target if @iot_dataa
+      if (count.to_i <= target.to_i && @iot_dataa.status = 'Processing')
+        puts "count start"
+        @iot_dataa.device_id = device_id
+        @iot_dataa.count = count
+        @iot_dataa.save! 
+        if @iot_dataa.save
+          seq_data_entry(@iot_dataa)
+        end
+        puts "count end"
+      else count.to_i == 0
+        redirect_to root_url, notice: 'Process Completed'
+      end
+
+      if count.to_i == target
+        @iot_dataa.status = 'Process Completed'
+        @iot_dataa.save!
+      end
+      puts "1 end"
+    end 
+
+    @iot_data_yts = IotDatum.where("status = ?", 'YTS')
+    @iot_data_pro = IotDatum.where("status = ?", 'Processing')
+    @iot_data_comp = IotDatum.where("status = ?", 'Process Completed')
+    
+    @active_devices = WorkbenchMaster.where("machine_status = ?", 'A')
+    @inactive_devices = WorkbenchMaster.where("machine_status = ?", 'IA')
+    @completed_parts = IotDatum.where("status = ?", 'Process Completed')
+    @processing_parts = IotDatum.where("status = ?", 'Processing')
+
+    @duration = IotDatum.select("(updated_at - created_at) AS duration").where("status = ?", 'Process Completed')
+
+    
+  end
+
+  # GET /iot_data/1
+  # GET /iot_data/1.json
+  def show
+  end
+
+  def download_report
+    puts params[:chart_id]
+    @@comp_rec = IotDatum.select("workbench_number, employee_name, shift, part_number, target, count, to_char(created_at, 'DD-MM-YYYY') as date, to_char(created_at, 'HH:MI:SS') as start_time, to_char(updated_at, 'HH:MI:SS') as end_time, to_char((updated_at - created_at),'HH24:MI:SS') as duration").where("status = ?", 'Process Completed')
+    respond_to do |format|
+      format.csv { send_data @@comp_rec.to_report_csv, filename: "ics_report-#{Date.today}.csv" }
+    end
+    
+  end
+
+  def seq_data_entry(data)
+    @tracker = Tracker.new
+    @tracker.wb_id = data.workbench_number
+    @tracker.part_code = data.part_number
+    @tracker.employee_id = data.employee_id
+    @tracker.shift = data.shift
+    @tracker.target = data.target
+    @tracker.device_id = data.device_id
+    @tracker.count = data.count
+    @tracker.save!
+  end
+
+
+  def process_start
+
+      id = params[:iot_datum_id]
+        @process_rec = IotDatum.find_by(id: id) if id
+        deviceId = @process_rec.device_id
+        part_number = @process_rec.part_number
+        
+        @iot_data = IotDatum.where("device_id = ? and status = ?", deviceId, 'YTS');
+        @uniq_data = IotDatum.where("device_id = ? and part_number = ? and status = ?", deviceId, part_number, 'Processing');
+        puts @uniq_data.count
+        puts "2 begin"
+        if @uniq_data.count == 0
+            @process_rec.status = 'Processing'
+            @process_rec.save!
+            redirect_to iot_data_path, notice: 'Process Started'
+            puts " 0 start"
+        else
+          @uniq_data.each do |u|
+            puts u.part_number
+            puts u.device_id
+            puts part_number
+            if (u.part_number == part_number && u.device_id == deviceId)
+              puts "Validate"
+              redirect_to iot_data_path, alert: "Part code #{part_number} is running on device No. #{deviceId}. Please Wait to complete"
+            else
+              puts "other part start----"
+              @process_rec.status = 'Processing'
+              @process_rec.save!
+              redirect_to iot_data_path, notice: 'Process Started'
+              puts "other part end----"
+            end
+          end
+        end
+     
+  end
+
+  def import
+    IotDatum.import(params[:file])
+    redirect_to iot_data_path, notice: 'Data imported.'
+  end
+
+  # GET /iot_data/new
+  def new
+    @iot_datum = IotDatum.new
+  end
+
+  # GET /iot_data/1/edit
+  def edit
+  end
+
+  # POST /iot_data
+  # POST /iot_data.json
+  def create
+    @iot_datum = IotDatum.new(iot_datum_params)
+
+    respond_to do |format|
+      if @iot_datum.save
+        format.html { redirect_to @iot_datum, notice: 'Iot datum was successfully created.' }
+        format.json { render :show, status: :created, location: @iot_datum }
+      else
+        format.html { render :new }
+        format.json { render json: @iot_datum.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /iot_data/1
+  # PATCH/PUT /iot_data/1.json
+  def update
+    respond_to do |format|
+      if @iot_datum.update(iot_datum_params)
+        format.html { redirect_to @iot_datum, notice: 'Iot datum was successfully updated.' }
+        format.json { render :show, status: :ok, location: @iot_datum }
+      else
+        format.html { render :edit }
+        format.json { render json: @iot_datum.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /iot_data/1
+  # DELETE /iot_data/1.json
+  def destroy
+    puts @iot_datum.count
+    if @iot_datum.count > 0
+      @deleted_rec = IotDatum.new
+      @deleted_rec.workbench_number = @iot_datum.workbench_number
+      @deleted_rec.part_number = @iot_datum.part_number
+      @deleted_rec.target = @iot_datum.target
+      @deleted_rec.lot_size = @iot_datum.lot_size
+      @deleted_rec.employee_name = @iot_datum.employee_name
+      @deleted_rec.shift = @iot_datum.shift
+      @deleted_rec.device_id = @iot_datum.device_id
+      @deleted_rec.count = @iot_datum.count
+      @deleted_rec.status = 'Deleted'
+      @deleted_rec.save!
+      @iot_datum.destroy
+    else
+      @iot_datum.destroy
+    end
+    respond_to do |format|
+      format.html { redirect_to iot_data_url, notice: 'Planner was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_iot_datum
+      @iot_datum = IotDatum.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def iot_datum_params
+      params.require(:iot_datum).permit(:workbench_number, :part_number, :target, :lot_size, :employee_name, :employee_id, :shift, :device_id, :count, :status)
+    end
+end
